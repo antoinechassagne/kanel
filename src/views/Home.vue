c44109a2673ce23cf34ea0448ed0628f397c6073
Antoine Chassagne <antoine.chassagne@me.com>
2019-11-28 13:02:33 +0100

    :building_construction: Remove generated files + update files structure

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,25 @@
 <template>
   <div>
+    <BaseHeader />
     <Heading level="1">Kanel</Heading>
-    <ButtonRouter path="about">Welcome</ButtonRouter>
+    <ButtonRouter path="/menus">Commencer</ButtonRouter>
     <ButtonMain @clicked="alert">click</ButtonMain>
+    <BaseFooter />
   </div>
 </template>
 
 <script>
-import Heading from '@/components/Heading';
-import ButtonRouter from '@/components/ButtonRouter';
-import ButtonMain from '@/components/ButtonMain';
+import BaseHeader from '@/components/base/BaseHeader';
+import BaseFooter from '@/components/base/BaseFooter';
+import Heading from '@/components/texts/Heading';
+import ButtonRouter from '@/components/buttons/ButtonRouter';
+import ButtonMain from '@/components/buttons/ButtonMain';
 
 export default {
   name: 'Home',
   components: {
+    BaseHeader,
+    BaseFooter,
     Heading,
     ButtonRouter,
     ButtonMain
