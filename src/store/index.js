39e279f91f84486c899ae73df1cf5f2b4823ec77
Antoine Chassagne <antoine.chassagne@me.com>
2019-12-12 11:35:19 +0100

    :sparkles: Init Vuex store

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,20 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import recommandations from '../settings/recommandations';
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  },
-});
+const state = {
+  recommandations,
+  weekMeals: {}
+};
+
+const getters = {
+  recommandations: state => state.recommandations,
+  weekMeals: state => state.weekMeals
+};
+
+const mutations = {};
+const actions = {};
+
+export default new Vuex.Store({ state, getters, mutations, actions });
