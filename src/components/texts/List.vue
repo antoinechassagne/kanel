270805c773b12c42eae0e48ae7530ef6f33c8ed0
Antoine Chassagne <antoine.chassagne@me.com>
2020-01-02 09:57:46 +0100

    :sparkles: Add vue fragments

--- src/components/texts/List.vue
+++ src/components/texts/List.vue
@@ -1,19 +1,23 @@
 <template>
-  <div>
+  <fragment>
     <h3>{{ label }}</h3>
     <ul>
       <li v-for="item in items" :key="generateUniqueComponentKey()">
         <slot v-bind:item="item"></slot>
       </li>
     </ul>
-  </div>
+  </fragment>
 </template>
 
 <script>
+import { Fragment } from 'vue-fragment';
 import generateUniqueComponentKey from '@/helpers/functions/generateUniqueComponentKey';
 
 export default {
   name: 'List',
+  components: {
+    Fragment
+  },
   props: {
     label: {
       type: String,
