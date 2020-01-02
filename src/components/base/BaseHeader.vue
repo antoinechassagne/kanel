80c97008d973a5452b71791fde8742f5f4268bd1
Antoine Chassagne <antoine.chassagne@me.com>
2020-01-02 21:02:52 +0100

    :lipstick: Add basic style on header

--- src/components/base/BaseHeader.vue
+++ src/components/base/BaseHeader.vue
@@ -1,6 +1,7 @@
 <template>
-  <nav>
-    <ul>
+  <nav class="header container">
+    <span class="header__title">KANEL</span>
+    <ul class="header__menu">
       <li><router-link to="/">Accueil</router-link></li>
       <li><router-link to="/week">Menu de la semaine</router-link></li>
     </ul>
