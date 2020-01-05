82856e0eb9f3f974e6007ca975cdf6413d14747c
Antoine Chassagne <antoine.chassagne@me.com>
2020-01-05 01:34:57 +0100

    :art: Improve semantic on header

--- src/components/base/BaseHeader.vue
+++ src/components/base/BaseHeader.vue
@@ -1,11 +1,13 @@
 <template>
-  <nav class="header container">
+  <header class="header container">
     <span class="header__title">KANEL</span>
-    <ul class="header__menu">
-      <li><router-link to="/">Accueil</router-link></li>
-      <li><router-link to="/week">Menu de la semaine</router-link></li>
-    </ul>
-  </nav>
+    <nav>
+      <ul class="header__menu">
+        <li><router-link to="/">Accueil</router-link></li>
+        <li><router-link to="/week">Menu de la semaine</router-link></li>
+      </ul>
+    </nav>
+  </header>
 </template>
 
 <script>
