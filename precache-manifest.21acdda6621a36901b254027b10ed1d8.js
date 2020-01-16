9e6fc3670981134a8dcee9e918ba402d37260bde
Antoine Chassagne <antoine.chassagne@me.com>
2020-01-16 09:32:54 +0100

    deploy

---
+++ precache-manifest.21acdda6621a36901b254027b10ed1d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37a77fa9cb58aa3da82b",
+    "url": "/kanel/css/app.8eae9d7b.css"
+  },
+  {
+    "revision": "0f8d9e8c06bfbcf99a59264f6a5a379f",
+    "url": "/kanel/img/home_background.jpg"
+  },
+  {
+    "revision": "27861fcab99710f5a49eadb05ee0966a",
+    "url": "/kanel/index.html"
+  },
+  {
+    "revision": "37a77fa9cb58aa3da82b",
+    "url": "/kanel/js/app.e189a7a9.js"
+  },
+  {
+    "revision": "3d4460eb796822c0e753",
+    "url": "/kanel/js/chunk-vendors.186198c1.js"
+  },
+  {
+    "revision": "481592b54949d01c95326235913221ff",
+    "url": "/kanel/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/kanel/robots.txt"
+  }
+]);
